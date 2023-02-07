450bbc0cc89af33562a09cd1120190b2db6be647
petryshynvladyslav <petryshynvladyslav@gmail.com>
2023-02-07 14:16:57 +0200

    saas

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-    body: document.querySelector("body"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-     refs.body.classList.toggle("no-scroll");
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
   }
 })();
